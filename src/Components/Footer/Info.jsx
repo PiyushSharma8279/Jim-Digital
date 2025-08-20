e340f180590b137be46647fbf10b0f542c9cc314
PiyushSharma8279 <piyush.vrwebconsulting@gmail.com>
2025-08-20 18:53:08 +0530

    changes

--- src/Components/Footer/Info.jsx
+++ src/Components/Footer/Info.jsx
@@ -6,42 +6,42 @@ function Info() {
             <div className="bg-[#0b0f28] text-white">
                 <div className="max-w-[1250px] mx-auto px-6 py-12 
                                 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-                    
+
                     {/* About */}
                     <div>
-                        <h2 className="underline py-2 text-[20px] font-medium">About</h2>
+                        <h2 className="underline decoration-2 underline-offset-8  py-2 text-[20px] font-medium">About</h2>
                         <p>
-                            Cybetiq is a forward-thinking IT company specializing in web 
-                            development, software design, and digital marketing. Our expert 
-                            team creates customized solutions to help businesses thrive and 
+                            Jim-digital is a forward-thinking IT company specializing in web
+                            development, software design, and digital marketing. Our expert
+                            team creates customized solutions to help businesses thrive and
                             enhance their online presence.
                         </p>
                         <div className=' flex gap-6 py-4'>
                             <FaInstagram size={25} />
-                        <FaFacebook size={25}/>
-                        <FaWhatsapp size={25}/>
-                        <FaLinkedin size={25}/>
+                            <FaFacebook size={25} />
+                            <FaWhatsapp size={25} />
+                            <FaLinkedin size={25} />
                         </div>
-                        
+
                     </div>
 
                     {/* Services */}
                     <div>
-                        <h2 className="underline py-2 text-[20px] font-medium">Our Services</h2>
+                        <h2 className="underline decoration-2 underline-offset-8  py-2 text-[20px] font-medium">Our Services</h2>
                         <div className="space-y-2">
                             <div><a href="">Web Development</a></div>
                             <div><a href="">Mobile App Development</a></div>
                             <div><a href="">Dedicated Resources</a></div>
                             <div><a href="">WordPress Development</a></div>
                             <div><a href="">Digital Marketing</a></div>
-                            <div> <a href="">SEO Service</a></div>  
-                           
+                            <div> <a href="">SEO Service</a></div>
+
                         </div>
                     </div>
 
                     {/* Get in Touch */}
                     <div>
-                        <h2 className="underline py-2 text-[20px] font-medium">Get In Touch</h2>
+                        <h2 className="underline decoration-2 underline-offset-8  py-2 text-[20px] font-medium">Get In Touch</h2>
                         <p className="py-1 font-semibold">Location</p>
                         <p className="py-1">
                             Australia Based. Results Driven.
@@ -51,16 +51,18 @@ function Info() {
                         <p className="py-1">Mail Us: info@jim-digital.com</p>
                     </div>
 
-                    {/* Useful Links */}
+
                     <div>
-                        <h2 className="underline py-2 text-[20px] font-medium">Useful Links</h2>
+                        <h2 className="underline decoration-2 underline-offset-8  py-2 text-[20px] font-medium">
+                            Useful Links
+                        </h2>
                         <div className="space-y-2">
                             <div><a href="">Company</a></div>
                             <div><a href="">Portfolio</a></div>
                             <div><a href="">Dedicated Resources</a></div>
                             <div><a href="">Blog</a></div>
                             <div><a href="">Career</a></div>
-                            <div><a href="">Contact Us</a></div> 
+                            <div><a href="">Contact Us</a></div>
                         </div>
                     </div>
                 </div>
--- src/Components/Home/Review.jsx
+++ src/Components/Home/Review.jsx
@@ -3,86 +3,136 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
+import { FaStar } from "react-icons/fa";
 
 function AutoSlider() {
   return (
     <>
-    <div className="w-full px-6 md:px-16 py-20">
-      <Swiper
-        modules={[Autoplay, Pagination]}
-        spaceBetween={20}
-        slidesPerView={3}
-        loop={true}
-        autoplay={{
-          delay: 2000,
-          disableOnInteraction: false,
-        }}
-        pagination={{ clickable: true }}
-        breakpoints={{
-          0: { slidesPerView: 1 },
-          640: { slidesPerView: 2 },
-          1024: { slidesPerView: 3 },
-        }}
-      >
-        {/* Box 1 */}
-        <SwiperSlide>
-          <div className=" border border-gray-300 p-6 rounded-xl  text-center">
-            <h2 className="text-xl font-bold">Rahul Sharma</h2>
-            <p className="text-sm mt-2">if you are only looking for a single-channel
+      <div className="w-full px-6 md:px-16 py-20">
+        <Swiper
+          modules={[Autoplay, Pagination]}
+          spaceBetween={20}
+          slidesPerView={3}
+          loop={true}
+          autoplay={{
+            delay: 2000,
+            disableOnInteraction: false,
+          }}
+          pagination={{ clickable: true }}
+          breakpoints={{
+            0: { slidesPerView: 1 },
+            640: { slidesPerView: 2 },
+            1024: { slidesPerView: 3 },
+          }}
+        >
+          {/* Box 1 */}
+          <SwiperSlide>
+            <div className=" border border-gray-300 p-6 rounded-xl  text-center">
+              
+              <p className="text-sm mt-2">If you are only looking for a single-channel
                 campaign, we can help you achieve your goals. If you need
                 direction, we can help you develop a digital strategy that grows
                 your business.</p>
-          </div>
-        </SwiperSlide>
+                <h2 className="text-xl font-bold">Rahul Sharma</h2>
+                <div className=" flex justify-center mt-2">
+                <div className="flex gap-1">
+                  <FaStar color="orange" />
+                  <FaStar color="orange" />
+                  <FaStar color="orange" />
+                  <FaStar color="orange" />
+                  <FaStar color="orange" />
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
 
-        {/* Box 2 */}
-        <SwiperSlide>
-          <div className="border border-gray-300  p-6 rounded-xl text-center">
-            <h2 className="text-xl font-bold">sunny sharma</h2>
-            <p className="text-sm mt-2">if you are only looking for a single-channel
+          {/* Box 2 */}
+          <SwiperSlide>
+            <div className="border border-gray-300  p-6 rounded-xl text-center">
+              
+              <p className="text-sm mt-2">If you are only looking for a single-channel
                 campaign, we can help you achieve your goals. If you need
                 direction, we can help you develop a digital strategy that grows
                 your business.</p>
-          </div>
-        </SwiperSlide>
+                <h2 className="text-xl font-bold">Sunny sharma</h2>
+              <div className=" flex justify-center mt-2">
+                <div className="flex gap-1">
+                  <FaStar color="orange" />
+                  <FaStar color="orange" />
+                  <FaStar color="orange" />
+                  <FaStar color="orange" />
+                  <FaStar color="orange" />
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
 
-        {/* Box 3 */}
-        <SwiperSlide>
-          <div className=" border border-gray-300 p-6 rounded-xl  text-center">
-            <h2 className="text-xl font-bold">Abhinav</h2>
-            <p className="text-sm mt-2">if you are only looking for a single-channel
+          {/* Box 3 */}
+          <SwiperSlide>
+            <div className=" border border-gray-300 p-6 rounded-xl  text-center">
+              <p className="text-sm mt-2">If you are only looking for a single-channel
                 campaign, we can help you achieve your goals. If you need
                 direction, we can help you develop a digital strategy that grows
                 your business.</p>
-          </div>
-        </SwiperSlide>
+                <h2 className="text-xl font-bold">Abhinav</h2>
+                <div className=" flex justify-center mt-2">
+                <div className="flex gap-1">
+                  <FaStar color="orange" />
+                  <FaStar color="orange" />
+                  <FaStar color="orange" />
+                  <FaStar color="orange" />
+                  <FaStar color="orange" />
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
 
-        {/* Box 4 */}
-        <SwiperSlide>
-          <div className=" border border-gray-300 p-6 rounded-xl  text-center">
-            <h2 className="text-xl font-bold">Deepak</h2>
-            <p className="text-sm mt-2">if you are only looking for a single-channel
+          {/* Box 4 */}
+          <SwiperSlide>
+            <div className=" border border-gray-300 p-6 rounded-xl  text-center">
+             
+              <p className="text-sm mt-2">If you are only looking for a single-channel
                 campaign, we can help you achieve your goals. If you need
                 direction, we can help you develop a digital strategy that grows
                 your business.</p>
-          </div>
-        </SwiperSlide>
+                 <h2 className="text-xl font-bold">Deepak</h2>
+                <div className=" flex justify-center mt-2">
+                <div className="flex gap-1">
+                  <FaStar color="orange" />
+                  <FaStar color="orange" />
+                  <FaStar color="orange" />
+                  <FaStar color="orange" />
+                  <FaStar color="orange" />
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
 
-        {/* Box 5 */}
-        <SwiperSlide>
-          <div className=" border border-gray-300 p-6 rounded-xl  text-center">
-            <h2 className="text-xl font-bold">Sam</h2>
-            <p className="text-sm mt-2">if you are only looking for a single-channel
+          {/* Box 5 */}
+          <SwiperSlide>
+            <div className=" border border-gray-300 p-6 rounded-xl  text-center">
+              
+              <p className="text-sm mt-2">If you are only looking for a single-channel
                 campaign, we can help you achieve your goals. If you need
                 direction, we can help you develop a digital strategy that grows
                 your business.</p>
-          </div>
-        </SwiperSlide>
-      </Swiper>
+                <h2 className="text-xl font-bold">Sam</h2>
+                <div className=" flex justify-center mt-2">
+                <div className="flex gap-1">
+                  <FaStar color="orange" />
+                  <FaStar color="orange" />
+                  <FaStar color="orange" />
+                  <FaStar color="orange" />
+                  <FaStar color="orange" />
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
 
-      {/* 🔲 Custom Pagination Styling */}
-     <style>
-        {`
+        
+        <style>
+          {`
           .swiper-pagination {
             bottom: -20px !important; /* ✅ push below slider */
             text-align: center; /* ✅ center align */
@@ -99,12 +149,12 @@ function AutoSlider() {
             background: #000;
           }
         `}
-      </style>
-    </div>
+        </style>
+      </div>
 
-    <div className=" text-center text-3xl font-medium py-6 bg-gray-200">
-        Still have questions? Call +61 426 443 970 for answers
-    </div>
+      <div className=" text-center text-xl lg:text-3xl font-medium py-6 bg-gray-200">
+        Still have questions? Call <span className=" font-bold text-blue-900 cursor-pointer">+61 426 443 970</span> for answers
+      </div>
 
     </>
   );
